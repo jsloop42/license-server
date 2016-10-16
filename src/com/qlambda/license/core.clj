aadfc855c83cc2fa5619791f4af93975cb823b22
kadaj <kadaj@qlambda.com>
2016-10-16 12:51:09 +0530

    all api responses in json

--- src/com/qlambda/license/core.clj
+++ src/com/qlambda/license/core.clj
@@ -21,7 +21,7 @@
      :headers {"Content-Type" "text/plain"}
      ;:body (json/generate-string {:publickey (apply str (map char @(utils/get-file-content (.getFile (io/resource pub-key-path)))))})})
      ;:body (json/generate-string {:publickey (slurp (io/resource pub-key-path))})})
-     :body (json/generate-string {:publickey (crypto/read-pub-key)})})
+     :body (json/generate-string {:publickey (crypto/read-pub-key) :status true})})
 
 (defroutes app-routes
     (GET "/" [] (resp/file-response "index.html" {:root "public"}))
@@ -30,7 +30,7 @@
         (GET "/crypto" [] (crypto/hello-crypto))
         (GET "/crypto/publickeyder" [] (get-publickey-der))
         (GET "/crypto/publickey" [] (get-publickey)))
-    (not-found "Page not found"))
+    (not-found (json/generate-string {:status false :msg "Page not found"})))
 
 (defn -main []
     (run-server (wrap-reload #'app-routes) {:port 8080})
